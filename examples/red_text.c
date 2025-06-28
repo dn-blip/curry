8651fe65ecd037fdcfaeb16318cc0d5eafd01531
dn-blip <nzekwe.da@wcs-g.com>
2025-06-28 13:38:58 -0600

    Update red_text.c
    I changed the API a bit last commit. Time to update the examples!

--- examples/red_text.c
+++ examples/red_text.c
@@ -8,15 +8,15 @@ int main(void) {
     // and platform. it has to be initialized seperately, as most functions either return
     // cr_error, the thing that was to be returned, or void.
     // cr_init() also fills the context with useful info about OS specifics.
-    cr_context *ctx;
-    cr_init(ctx);
+    cr_context ctx;
+    cr_init(&ctx);
 
-    cr_state initial_state = cr_get_state(*ctx);
+    cr_state initial_state = cr_get_state(&ctx);
 
     // To set colors, you kinda have to pass the memory for the color buffer.
 
     unsigned int rgb[3] = {255, 0, 0};
-    cr_error err = cr_set_color(ctx, rgb);
+    cr_error err = cr_set_color(&ctx, rgb);
 
     if (err != CR_TERM_SUCCESS) {
         fprintf(stderr, "Error setting color: %s\n", err.message);
