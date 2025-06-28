5d7f7f81d255286fa79581610e36b81d8f2726ad
dn-blip <nzekwe.da@wcs-g.com>
2025-06-28 19:25:50 +0000

    TODO: Set console attributes in Win32.

--- curry.h
+++ curry.h
@@ -1,158 +1,121 @@
 #ifndef CURRY_H
 #define CURRY_H
 
-#ifdef __cplusplus
-
-extern "C" {
 #include <stdbool.h>
+#include <stdint.h>
 
 // - Macros - //
 #ifndef CURRY_API
 #define CURRY_API static
 #endif
 
-#ifndef curry_malloc(sz)
+#ifndef curry_malloc
 #define curry_malloc(sz) malloc(sz)
 #endif
 
 // - Types and Enums - //
 
-enum cr_platform {
+typedef enum {
 	CURRY_TERM_PLATFORM_UNKNOWN = 0,
 	CURRY_TERM_PLATFORM_WINDOWS,
 	CURRY_TERM_PLATFORM_LINUX,
 	CURRY_TERM_PLATFORM_MACOS,
 	CURRY_TERM_PLATFORM_BSD
-};
+} cr_platform;
 
-typedef enum cr_platform cr_platform;
-
-enum cr_term_errors {
+typedef enum {
 	CURRY_TERM_SUCCESS = 0,
 	CURRY_TERM_ERROR_INIT_FAILED,
 	CURRY_TERM_ERROR_INVALID_STATE,
 	CURRY_TERM_ERROR_INVALID_SIZE,
 	CURRY_TERM_ERROR_INVALID_COLOR,
 	CURRY_TERM_ERROR_UNKNOWN_PLATFORM
-};
+} cr_term_errors;
 
-struct cr_error {
+typedef struct {
 	cr_term_errors short_form;
-	const char message[512];
-};
+	char message[512];
+} cr_error;
 
-typedef struct cr_error cr_error;
+typedef enum {
+	CURRY_TERM_ECHO_ON = 0,
+	CURRY_TERM_ECHO_OFF,
+	CURRY_TERM_CURSOR_VISIBLE,
+	CURRY_TERM_MOUSE_ENABLED,
+	CURRY_TERM_MOUSE_DISABLED,
+	CURRY_TERM_COLOR_DEPTH,
+} cr_attribute;
 
-enum cr_attribute {
-	CURRY_TERM_ECHO_ON = 0, // Echo input
-	CURRY_TERM_ECHO_OFF, // No echo input
-	CURRY_TERM_CURSOR_VISIBLE, // Cursor visibility
-	CURRY_TERM_MOUSE_ENABLED, // Mouse support enabled
-	CURRY_TERM_MOUSE_DISABLED, // Mouse support disabled
-	CURRY_TERM_COLOR_DEPTH, // Color depth of the terminal
-};
+typedef struct {
+	int platform_handle;
+	bool initialized;
+	bool mouse_enabled;
+	bool mouse_pressed;
+	cr_attribute attributes;
+	unsigned int size;
+	unsigned int color_depth;
+	unsigned int width, height;
+} cr_state;
 
-typedef enum cr_attribute cr_attribute;	
-
-// This following struct also holds platform-specific handles. There's an int here, for generallity.
-struct cr_state {
-	int platform_handle; // Platform-specific handle (e.g., file descriptor, window handle)
-	bool initialized; // Is the terminal initialized?
-	bool mouse_enabled; // Is mouse support enabled?
-	bool mouse_pressed; // Is the mouse pressed?
-	cr_attribute attributes; // Terminal attributes
-	unsigned int size; // Size of the terminal in characters
-	unsigned int color_depth; // Color depth of the terminal
-	unsigned int width, height; // Width and height of the terminal in characters
-};
-
-typedef struct cr_state cr_state;
-
-// The context hold properties of the terminal, which should be filled using curry_term_init().
-// The context passes information about the terminal to library functions. 
-// It also holds color information, which is used to determine how to render colors in the terminal.
-struct cr_context {
+typedef struct {
 	cr_platform platform;
 	cr_state state;
-	int width, height; 
-};
+	int width, height;
+} cr_context;
 
-typedef struct cr_context cr_context;
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 // - Functions - //
 
 CURRY_API cr_error cr_init(cr_context *ctx);
 
-CURRY_API cr_error cr_get_attribute(cr_context ctx, cr_attribute attribute, void* value);
+CURRY_API cr_error cr_get_attribute(cr_context *ctx, cr_attribute attribute, void* value);
 
-CURRY_API unsigned int cr_get_size(cr_context ctx);
+CURRY_API unsigned int cr_get_size(cr_context *ctx);
 
 CURRY_API cr_error cr_set_size(cr_context* ctx, uint32_t width, uint32_t height);
 
 CURRY_API cr_error cr_set_color(cr_context* ctx, const int rgb[3]);
 
-// curry_term_state_t curry_term_reset(curry_term_context* ctx);
+#ifdef __cplusplus
+}
+#endif
 
-
-} // extern "C"
-#endif // __cplusplus
 #endif // CURRY_H
 
-#ifndef CURRY_IMPLEMENTATION
-#define CURRY_IMPLEMENTATION
+// Implementation section
+#ifdef CURRY_IMPLEMENTATION
+
+#include <stdarg.h>
 
 #ifdef __cplusplus
 extern "C" {
-#include <stdint.h>
-#include <stdarg.h>
-#include <stdbool.h>
+#endif
 
 CURRY_API cr_error cr_init(cr_context *ctx) {
-	// Initialize the context based on the platform
 	if (!ctx) {
-		return (cr_error){CURRY_TERM_ERROR_INIT_FAILED, "Context is NULL"};
-	}
+		cr_error err = { CURRY_TERM_ERROR_INIT_FAILED, "Context pointer is null." };
+		return err
 
 	#ifdef _WIN32
 	#include <windows.h>
-
-	ctx->platform = CURRY_TERM_PLATFORM_WINDOWS;
-	ctx->state.platform_handle = GetStdHandle(STD_OUTPUT_HANDLE);
-	if (ctx->state.platform_handle == INVALID_HANDLE_VALUE) {
-		return (cr_error){CURRY_TERM_ERROR_INIT_FAILED, "Failed to get standard output handle"};
-	}
-	ctx->state.initialized = true;
-	ctx->state.mouse_enabled = false;
-	ctx->state.mouse_pressed = false;
-	ctx->state.attributes = CURRY_TERM_ECHO_ON; // Default attribute
-
-	#elif defined(__linux__)
-	#include <unistd.h>
-	#include <termios.h>
-
-	ctx->state.platform_handle = STDOUT_FILENO; // Standard output file descriptor
-	if (ctx->state.platform_handle < 0) {
-		return (cr_error){CURRY_TERM_ERROR_INIT_FAILED, "Failed to get standard output file descriptor"};
-	}
-	ctx->state.initialized = true;
-	ctx->state.mouse_enabled = false;
-	ctx->state.mouse_pressed = false;
-	ctx->state.attributes = CURRY_TERM_ECHO_ON; // Default attribute
-
-	ctx->platform = CURRY_TERM_PLATFORM_LINUX;
-
-
-	#elif defined(__APPLE__)
-	ctx->platform = CURRY_TERM_PLATFORM_MACOS;
-	#elif defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__)
-	ctx->platform = CURRY_TERM_PLATFORM_BSD;
-	#else
-	ctx->platform = CURRY_TERM_PLATFORM_UNKNOWN;
-		return (cr_error){CURRY_TERM_ERROR_UNKNOWN_PLATFORM, "Unknown platform"};
+		ctx->platform = CURRY_TERM_PLATFORM_WINDOWS;
+		HANDLE hConsoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+		if (hConsoleHandle == INVALID_HANDLE_VALUE) {
+			cr_error err = { CURRY_TERM_ERROR_INIT_FAILED, "Failed to get console handle." };
+			return err;
+		}
+		ctx->state.platform_handle = (int)hConsoleHandle;
+		ctx->state.initialized = true;
+		
+		// TODO: Set attributes using GetConsoleMode or SetConsoleMode
 	#endif
-
-	return (cr_error){CURRY_TERM_SUCCESS, "Initialization successful"};
-
 }
-#endif // __cplusplus
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif // CURRY_IMPLEMENTATION
